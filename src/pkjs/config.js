62ddb2d81770b946a7b2f3ae509330cff02fa334
Sergey Alyaev <cobxo3bot@gmail.com>
2016-12-30 18:46:05 +0100

    Back-up

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -5,48 +5,48 @@ module.exports = [
   },
   {
     "type": "text",
-    "defaultValue": "Here is some introductory text."
+    "defaultValue": ""
   },
   {
     "type": "section",
     "items": [
-      {
-        "type": "heading",
-        "defaultValue": "Colors"
-      },
-      {
-        "type": "color",
-        "messageKey": "BackgroundColor",
-        "defaultValue": "0x000000",
-        "label": "Background Color"
-      },
-      {
-        "type": "color",
-        "messageKey": "ForegroundColor",
-        "defaultValue": "0xFFFFFF",
-        "label": "Foreground Color"
-      }
+//       {
+//         "type": "heading",
+//         "defaultValue": "Places to follow"
+//       },
+//       {
+//         "type": "color",
+//         "messageKey": "BackgroundColor",
+//         "defaultValue": "0x000000",
+//         "label": "Background Color"
+//       },
+//       {
+//         "type": "color",
+//         "messageKey": "ForegroundColor",
+//         "defaultValue": "0xFFFFFF",
+//         "label": "Foreground Color"
+//       }
     ]
   },
   {
     "type": "section",
     "items": [
-      {
-        "type": "heading",
-        "defaultValue": "More Settings"
-      },
-      {
-        "type": "toggle",
-        "messageKey": "SecondTick",
-        "label": "Enable Seconds",
-        "defaultValue": false
-      },
-      {
-        "type": "toggle",
-        "messageKey": "Animations",
-        "label": "Enable Animations",
-        "defaultValue": false
-      }
+//       {
+//         "type": "heading",
+//         "defaultValue": "More Settings"
+//       },
+//       {
+//         "type": "toggle",
+//         "messageKey": "SecondTick",
+//         "label": "Enable Seconds",
+//         "defaultValue": false
+//       },
+//       {
+//         "type": "toggle",
+//         "messageKey": "Animations",
+//         "label": "Enable Animations",
+//         "defaultValue": false
+//       }
     ]
   },
   {
